a53f98d4be1805d09d89ea8e5d6753098a55d952
cyrdam <51902761+cyrdam@users.noreply.github.com>
2024-06-26 13:41:30 +0200

    fix: unit tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -5,14 +5,14 @@ import { App } from './app';
 describe('App', () => {
 	test('should render App', () => {
 		const { container } = render(<App />);
-		expect(container.textContent).contains('Vite + Preactcount is Edit');
+		expect(container.textContent).contains('Vite + Preact');
 		expect(container).toMatchSnapshot();
 	});
 
 	test('should increment after "count is" button is clicked', async () => {
 		render(<App />);
 
-		fireEvent.click(screen.getByText('count is'));
+		fireEvent.click(screen.getByText('count is 0'));
 		await waitFor(() => {
 			// .toBeInTheDocument() is an assertion that comes from jest-dom.
 			// Otherwise you could use .toBeDefined().
